b6d43f6b083c1c43a5a7f7057a32bf46895c094b
abbabagal <bagaltushar09@gmail.com>
2024-08-07 20:04:53 +0530

    changed the url from http to https

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import MovieCard from "./MovieCard";
 import SearchIcon from "./search.svg";
 
-const API_URL = "http://www.omdbapi.com/?apikey=e411281&";
+const API_URL = "https://www.omdbapi.com/?apikey=e411281&";
 
 const App = () => {
   const [movies, setmovies] = useState([]);
